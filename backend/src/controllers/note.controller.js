4638fb9b0c5be1a8437a62c703202f21c5142b78
ShaikAbbuSofiyan <sksofiyan004@gmail.com>
2026-02-20 14:49:56 +0530

    Notes controllers added successfully

--- backend/src/controllers/note.controller.js
+++ backend/src/controllers/note.controller.js
@@ -18,13 +18,57 @@ async function createNote(req, res) {
 }
 
 async function editNote(req, res) {
+    try {
+        const { heading, content } = req.body;
+        const user = req.user;
 
+        const { id } = req.params; 
+
+        const note = await noteModel.findById(id);
+
+        if (!note) {
+            return res.status(400).json({
+                message: "Invalid Note"
+            });
+        }
+
+        const newNote = await noteModel.findByIdAndUpdate(
+            id,
+            { heading, content },
+            { new: true } 
+        );
+
+        res.status(200).json({
+            message: "Note updated successfully",
+            newNote
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
-
 async function deleteNote(req, res) {
+    try {
+        const { id } = req.params;
 
+        const deletedNote = await noteModel.findByIdAndDelete(id);
+
+        if (!deletedNote) {
+            return res.status(404).json({
+                message: "Note not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Note deleted successfully"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
 }
-
 module.exports = {
     createNote,
     editNote,
